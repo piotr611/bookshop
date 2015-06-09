fd86426818c937a18aff688d078a1885377e2bd0
piotr611 <piotrgorbunov@mail.ru>
2015-06-09 00:23:33 -0400

    Validation and blog tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :price, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true, length: {minimum: 3}
+	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'URL have to include GIF, JPG or PNG formats'}
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,70 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+
+    def new_product(image_url)
+    	Product.new(	title: "my title",
+    					description: "me description",
+    					price: 99.99,
+    					image_url: image_url)
+    	
+    end
+
+    test "product attribute must not be empty" do
+      product = Product.new
+      assert product.invalid?
+      assert product.errors[:title].any?
+      assert product.errors[:description].any?
+      assert product.errors[:price].any?
+    end
+
+    test "product price must be positive" do
+    	product = Product.new(  title: "my title t",
+    							description: "my description t")
+
+    	product.price = -2
+    	assert product.invalid? 
+    	#assert_equal ["must be griter or equal 0.01"], product.errors[:price]
+
+    	product.price = 0
+    	assert product.invalid?
+    	#assert_equal ["must be griter or equal 0.01"], product.errors[:price]
+
+    	product.price = 1
+    	assert product.valid?
+
+    end
+
+    test "image url" do 
+    	product = Product.new(	title: "my title",
+    							description: "me description",
+    							price: 99.99)
+    	ok = %w{1.gif 2.jpg 3.png}
+    	bad = %w{1.doc 2.gif123 3.gif.qwe}
+
+    	ok.each do |name|
+    		assert new_product(name).valid?, "#{name} should not be invalid"
+    	end
+
+    	bad.each do |name|
+    		assert new_product(name).invalid?, "#{name} should not be valid"
+    	end 
+
+	end
+
+	test "product is not valid without an unique title" do
+		product = Product.new(	title: products(:true_data).title,
+								description: "me description too",
+    							price: 99.99)
+		assert product.invalid?
+	end
+
+	test "very small title" do
+		product = Product.new( title: products(:false_title_only_2_symbols).title,
+								description: "me description too",
+    							price: 99.99)
+		assert product.invalid?
+	end
+
 end
