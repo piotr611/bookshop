b53c09a0b278cb1caaaba7c8ac8bc2c08855e9d8
piotr611 <piotrgorbunov@mail.ru>
2015-06-09 16:39:40 -0400

    add controller for user and functionals tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def my_time_now
+  	@time = Time.now
+  	@time.strftime("%b %-d,%Y")
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true, length: {minimum: 3}
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'URL have to include GIF, JPG or PNG formats'}
+
+	def self.latest
+		Product.order(:update_at).last
+	end	
+
 end
